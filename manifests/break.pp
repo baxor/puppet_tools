a8a170b35ddf0ec37675d57765681914a7c70c6c
Adam Backer <backer@uber.com>
2015-01-16 11:34:26 -0800

    title, not 'continue'

--- manifests/break.pp
+++ manifests/break.pp
@@ -1,6 +1,6 @@
 define common::break($receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
   exec { "break_for_${title}":
-    command => "bash -c 'while [ ! -e ${receiptdir}/${title} ]; do sleep 2; done && rm -f ${receiptdir}/continue'"
+    command => "bash -c 'while [ ! -e ${receiptdir}/${title} ]; do sleep 2; done && rm -f ${receiptdir}/${title}'"
     path => $path,
     creates => "${receiptdir}/${title}",
     require => File[$receiptdir],
