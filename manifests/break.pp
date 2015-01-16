28cc975ae8af0bd5e7c5acc53747b6b615bcd9ef
Adam Backer <backer@uber.com>
2015-01-16 11:47:11 -0800

    add some comments

--- manifests/break.pp
+++ manifests/break.pp
@@ -1,4 +1,5 @@
 define common::break($timeout=120, $sleep=2, $receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
+  #universal compatible:
   exec { "break_for_${title}":
     command => "bash -c 'while [ ! -e ${receiptdir}/${title} ]; do sleep ${sleep}; done && rm -f ${receiptdir}/${title}'"
     path => $path,
@@ -6,7 +7,7 @@ define common::break($timeout=120, $sleep=2, $receiptdir=$common::receipt_dir, $
     creates => "${receiptdir}/${title}",
     require => File[$receiptdir],
   }
-  #Alternate:
+  #Alternate (puppet 3+):
 #define common::break($tries=30, $sleep=2, $receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
 #  exec { "break_for_${title}":
 #    command => "ls ${receiptdir}/${title} && rm -f ${receiptdir}/${title}",
