fd294af953ed2f5547845f2ab6883f01541bfc2f
Adam Backer <backer@uber.com>
2015-01-16 11:46:40 -0800

    add timeout, expose timeout and sleep as arguments, add modern alternative using tries/try_sleep

--- manifests/break.pp
+++ manifests/break.pp
@@ -1,8 +1,18 @@
-define common::break($receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
+define common::break($timeout=120, $sleep=2, $receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
   exec { "break_for_${title}":
-    command => "bash -c 'while [ ! -e ${receiptdir}/${title} ]; do sleep 2; done && rm -f ${receiptdir}/${title}'"
+    command => "bash -c 'while [ ! -e ${receiptdir}/${title} ]; do sleep ${sleep}; done && rm -f ${receiptdir}/${title}'"
     path => $path,
+    timeout => $timeout, 
     creates => "${receiptdir}/${title}",
     require => File[$receiptdir],
   }
+  #Alternate:
+#define common::break($tries=30, $sleep=2, $receiptdir=$common::receipt_dir, $path='/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin') {
+#  exec { "break_for_${title}":
+#    command => "ls ${receiptdir}/${title} && rm -f ${receiptdir}/${title}",
+#    path => $path, 
+#    tries => $tries, 
+#    try_sleep => $sleep, 
+#    require => File[$receiptdir],
+#  }
 }
